0b88083fb3f9d499699f55e3899ffb41b7808944
angusmiller28 <angus.miller28@gmail.com>
2018-01-16 20:03:41 +1000

    Improved frame to allow easy configure of camera position

--- CameraFramework/CameraViewController.swift
+++ CameraFramework/CameraViewController.swift
@@ -9,19 +9,30 @@
 import UIKit
 import AVFoundation
 
+public enum CameraPosition {
+    case front
+    case back
+}
+
 public final class CameraViewController: UIViewController {
+    public var position: CameraPosition = .back
+    
     var session = AVCaptureSession()
     var discoverySession:AVCaptureDevice.DiscoverySession? {
         return AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInDualCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
         }
+    var videoInput: AVCaptureDeviceInput?
     var videoOutput = AVCaptureVideoDataOutput()
     
     public init(){
         super.init(nibName: nil, bundle: nil)
+    }
+    
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         createUI()
         commitConfiguration()
     }
-    
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -29,16 +40,17 @@ public final class CameraViewController: UIViewController {
     func getPreviewLayer(session: AVCaptureSession) -> AVCaptureVideoPreviewLayer {
         let previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
         previewLayer.frame = self.view.bounds
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
         return previewLayer
     }
     
-    func getDevice() -> AVCaptureDevice? {
+    func getDevice(with position: AVCaptureDevice.Position) -> AVCaptureDevice? {
         guard let discoverySession = self.discoverySession else {
             return nil
         }
         
         for device in discoverySession.devices {
-            if device.position == AVCaptureDevice.Position.back {
+            if device.position == position {
                 return device
             }
         }
@@ -61,14 +73,19 @@ public final class CameraViewController: UIViewController {
     }
     
     func commitConfiguration(){
+        if let currentInput = self.videoInput {
+            self.session.removeInput(currentInput)
+            self.session.removeOutput(self.videoOutput)
+        }
         do {
-            guard let device = getDevice() else {
+            guard let device = getDevice(with: self.position == .front ? AVCaptureDevice.Position.front : AVCaptureDevice.Position.back) else {
                 return
             }
             let input = try AVCaptureDeviceInput(device: device)
             if self.session.canAddInput(input) &&
                self.session.canAddOutput(self.videoOutput)
             {
+                self.videoInput = input
                 self.session.addInput(input)
                 self.session.addOutput(self.videoOutput)
                 self.session.commitConfiguration()
