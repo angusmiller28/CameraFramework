fc853aa91846cb0b2ffc0b5f16b560bba93eac27
angusmiller28 <angus.miller28@gmail.com>
2018-01-16 18:17:47 +1000

    Added UI and configuration code to framework

--- CameraFramework/CameraViewController.swift
+++ CameraFramework/CameraViewController.swift
@@ -18,21 +18,12 @@ public final class CameraViewController: UIViewController {
     
     public init(){
         super.init(nibName: nil, bundle: nil)
+        createUI()
+        commitConfiguration()
     }
     
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    override public func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override public func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func getPreviewLayer(session: AVCaptureSession) -> AVCaptureVideoPreviewLayer {
@@ -52,6 +43,41 @@ public final class CameraViewController: UIViewController {
             }
         }
         return nil
+    }
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override public func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func createUI(){
+        self.view.layer.addSublayer(getPreviewLayer(session: self.session))
+    }
+    
+    func commitConfiguration(){
+        do {
+            guard let device = getDevice() else {
+                return
+            }
+            let input = try AVCaptureDeviceInput(device: device)
+            if self.session.canAddInput(input) &&
+               self.session.canAddOutput(self.videoOutput)
+            {
+                self.session.addInput(input)
+                self.session.addOutput(self.videoOutput)
+                self.session.commitConfiguration()
+                self.session.startRunning()
+            }
+        } catch {
+            print("Error linking device to AVInput!")
+            return
+        }
     }
 
     /*
