7c6c0998909b6c18022242fc16d633085ebeba11
angusmiller28 <angus.miller28@gmail.com>
2018-01-16 17:37:57 +1000

    Initial Commit

---
+++ CameraFramework/CameraFramework.h
@@ -0,0 +1,19 @@
+//
+//  CameraFramework.h
+//  CameraFramework
+//
+//  Created by Angus Miller on 16/1/18.
+//  Copyright © 2018 Angus Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CameraFramework.
+FOUNDATION_EXPORT double CameraFrameworkVersionNumber;
+
+//! Project version string for CameraFramework.
+FOUNDATION_EXPORT const unsigned char CameraFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraFramework/PublicHeader.h>
+
+
